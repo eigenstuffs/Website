f010e76625104e74e31dad1f22a31d2dc0a353c0
branden <88228182+eigenstuffs@users.noreply.github.com>
2025-04-01 10:06:32 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    height: 80vh;
     overflow: hidden;
   }
 
@@ -120,15 +120,24 @@
         <path d="M16,2.345c7.735,0,14,6.265,14,14-.002,6.015-3.839,11.359-9.537,13.282-.7,.14-.963-.298-.963-.665,0-.473,.018-1.978,.018-3.85,0-1.312-.437-2.152-.945-2.59,3.115-.35,6.388-1.54,6.388-6.912,0-1.54-.543-2.783-1.435-3.762,.14-.35,.63-1.785-.14-3.71,0,0-1.173-.385-3.85,1.435-1.12-.315-2.31-.472-3.5-.472s-2.38,.157-3.5,.472c-2.677-1.802-3.85-1.435-3.85-1.435-.77,1.925-.28,3.36-.14,3.71-.892,.98-1.435,2.24-1.435,3.762,0,5.355,3.255,6.563,6.37,6.913-.403,.35-.77,.963-.893,1.872-.805,.368-2.818,.963-4.077-1.155-.263-.42-1.05-1.452-2.152-1.435-1.173,.018-.472,.665,.017,.927,.595,.332,1.277,1.575,1.435,1.978,.28,.787,1.19,2.293,4.707,1.645,0,1.173,.018,2.275,.018,2.607,0,.368-.263,.787-.963,.665-5.719-1.904-9.576-7.255-9.573-13.283,0-7.735,6.265-14,14-14Z"></path>
       </svg>
     </a>
-    <a href="mailto:brandenb@ucla.edu" target="_blank" rel="noopener noreferrer">
+    <a href="mailto:bohrnsen@umich.edu" target="_blank" rel="noopener noreferrer">
       <svg class="icon-link" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
         <path d="M29.44,16.318c0-.993-.089-1.947-.255-2.864h-13.185v5.422h7.535c-.331,1.744-1.324,3.22-2.813,4.213v3.525h4.544c2.647-2.444,4.175-6.033,4.175-10.296Z" opacity=".4"></path><path d="M16,30c3.78,0,6.949-1.247,9.265-3.385l-4.544-3.525c-1.247,.84-2.838,1.349-4.722,1.349-3.64,0-6.733-2.456-7.84-5.765l-2.717,2.09-1.941,1.525c2.304,4.569,7.025,7.713,12.498,7.713Z"></path><path d="M8.16,18.66c-.28-.84-.445-1.731-.445-2.66s.165-1.82,.445-2.66v-3.615H3.502c-.955,1.884-1.502,4.009-1.502,6.275s.547,4.391,1.502,6.275h3.332s1.327-3.615,1.327-3.615Z" opacity=".4"></path><path d="M16,7.575c2.062,0,3.895,.713,5.358,2.087l4.009-4.009c-2.431-2.265-5.587-3.653-9.367-3.653-5.473,0-10.195,3.144-12.498,7.725l4.658,3.615c1.107-3.309,4.2-5.765,7.84-5.765Z"></path>
       </svg>
     </a>
   </div>
   <p>
-    I'm Branden, and I do research and game development.<br>
-    Here, you can find my latest work. Enjoy your stay!
+    Welcome! My name is Branden and I'm a Public Policy & Political Science PhD student at the University of Michigan.<br><br>
+
+    My current interests are in (1) the political behavior of economic actors amidst the
+    clean energy transition and (2) electoral competition.<br><br>
+
+    Methodologically, I am interested in spatial data, text, and networks.<br><br>
+
+    Prior to my PhD, I graduated from UCLA with Highest Honors in Political Science. Aside from research,
+    I develop independent video games, which you can also find here.<br><br>
+
+    You can find my recent work and CV below. Feel free to reach out at any time; my contact information is above. Enjoy your stay!
   </p>
   <div class="button-row">
     <button on:click={() => (window.location.href = '/research')}>Research</button>
